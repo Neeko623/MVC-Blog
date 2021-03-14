9180bbd94a2d82fb4da98a872b6061e7113f70a9
Neeko Tang <neeko_tvxq@hotmail.com>
2021-03-14 15:10:58 -0400

    update seed id to database

--- seeds/comment-seeds.js
+++ seeds/comment-seeds.js
@@ -3,18 +3,18 @@ const { Comment } = require('../models');
 const commentdata = [
   {
     comment_text: 'What is JavaScript?',
-    user_id: 1,
-    post_id: 1
+    user_id: 4,
+    post_id: 34
   },
   {
     comment_text: 'What is the use of isNaN function?',
-    user_id: 2,
-    post_id: 2
+    user_id: 24,
+    post_id: 24
   },
   {
     comment_text: 'What is the difference between ViewState and SessionState?',
-    user_id: 3,
-    post_id: 3
+    user_id: 14,
+    post_id: 74
   },
 ];
 
